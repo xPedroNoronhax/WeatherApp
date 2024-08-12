aa21912826b74acc9ebd6fa5bdc72c41ff6ee7f6
Pedro Noronha <pedro_noronha@outlook.com>
2024-08-12 14:12:40 -0300

    adding first container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ import Navbar from "@/components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { WeatherData } from "../../types";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale"; // Importando o locale para português
+import Container from "@/components/Container";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import WeatherIcon from "@/components/WeatherIcon";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
 
 export default function Home() {
   const { isLoading, error, data } = useQuery<WeatherData>(
@@ -16,8 +22,8 @@ export default function Home() {
       return data;
     }
   );
-
-  console.log(data);
+  const firstData = data?.list[0];
+  console.log(data?.city);
 
   if (isLoading)
     return (
@@ -25,13 +31,66 @@ export default function Home() {
         <p className="animate-bounce">Carregando...</p>
       </div>
     );
+
+  // Função para capitalizar a primeira letra
+  const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const dayOfWeek = format(parseISO(firstData?.dt_txt ?? ""), "EEEE", {
+    locale: ptBR,
+  });
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="px-3 max-2-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
-        <section>
-          <div>
-            <h2 className="flex gap-1 text-2xl items-center"></h2>
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{capitalizeFirstLetter(dayOfWeek)}</p>
+              <p className="text-lg">
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4 ">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 0)}°
+                </span>
+                <p className="txt-xs space-x-1 whitespace-nowrap">
+                  <span>Sensação de:</span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}°↓
+                  </span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}°↑
+                  </span>
+                </p>
+              </div>
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(d.dt_txt), "HH:mm ")}
+                    </p>
+                    {/* <WeatherIcon iconName={d.weather[0].icon} /> */}
+                    <WeatherIcon
+                      iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)}
+                    />
+                    <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
+                  </div>
+                ))}
+              </div>
+            </Container>
           </div>
         </section>
         <section></section>
--- types.ts
+++ types.ts
@@ -1,57 +1,54 @@
-// weatherDataTypes.ts
-
-export type Weather = {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-};
-
-export type Main = {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  humidity: number;
-  sea_level: number;
-  grnd_level: number;
-};
-
-export type Wind = {
-  speed: number;
-  deg: number;
-};
-
-export type Clouds = {
-  all: number;
-};
-
-export type Sys = {
-  type: number;
-  id: number;
-  country: string;
-  sunrise: number;
-  sunset: number;
-};
-
-export type Coord = {
-  lon: number;
-  lat: number;
-};
-
-export type WeatherData = {
-  coord: Coord;
-  weather: Weather[];
-  base: string;
-  main: Main;
-  visibility: number;
-  wind: Wind;
-  clouds: Clouds;
+export interface WeatherDetail {
   dt: number;
-  sys: Sys;
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
-};
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+export interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
